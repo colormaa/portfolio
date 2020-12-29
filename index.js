d89c7bd13f7e5f4f93a21fb4ff0fadb64d44ca53
Bolormaa <crustalmaa@gmail.com>
2020-12-28 23:32:06 -0500

    commit22

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.port|| 3000
 app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
